1f6362791f25234aedae48e5e4e9a89f83a93b63
akasimova <brskasimova@gmail.com>
2021-07-21 21:25:50 +0300

    Ex7: Запросы и методы

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-from random import choice
-
-url = "https://playground.learnqa.ru/api/compare_query_type"
-list_methods = ["GET", "POST", "PUT", "DELETE"]
-method = choice(list_methods)
-print(method)
-
-
